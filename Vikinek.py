022ab0c35bc35b0452304d10d99a0986ef8d9ece
Hekate666 <71078572+Hekate666@users.noreply.github.com>
2020-09-14 12:39:44 +0200

    Add files via upload

---
+++ Vikinek.py
@@ -0,0 +1,7 @@
+Lépj be githubba
+Kattints bal oldalon fent a macskára
+Start a project gombra katt
+Repository name  - adj nevet a projectnek
+Alul zöld gomb: Create repository
+Quick step alatt kék linkkel: Get started by creating a new file or uploading an existing file. We recommend every repository include a README, LICENSE, and .gitignore.
+Ebből válaszd az uploadingot
